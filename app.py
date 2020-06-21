18b90378ede96c7e61c9d47c75f6f395536b0ff8
Jo-Seungjun <cfi02222@gmail.com>
2020-06-21 18:40:19 +0900

    renew html and app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from memo import Memo
+from memo import MemoManager
 import time
 
 from flask import Flask
@@ -7,72 +7,83 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 
-memo = Memo()
+memoManager = MemoManager()
 app = Flask(__name__)
 
+@app.route('/list', methods=['GET'])
 def memo_list():
-    return " 메모 목록과 프리뷰만 보여주는 UI "
+    memo_list = memo.get_memo_list()
+    return render_template('list.html', memo_list=memo_list)
+
+@app.route('/viewer', methods=['GET'])
+def viewer():
+    memo_id = request.args.get('id')
+    memo = memo.get_memo_content(memo_id)
+    return render_template('viewer.html', memo=memo)
 
 @app.route('/')
 def main():
     return redirect(url_for(get_memo_list))
 
-@app.route('/create_memo', methods=['GET'])
+@app.route('/memo', methods=['POST'])
 def create_memo():
     # request 에서 json 가져오기 https://stackoverflow.com/questions/20001229/how-to-get-posted-json-in-flask
-    id_ = time.time()
-    head = dict()
-    head["id"] = id_
-    head["title"] = ""
-    head["writer"] = "temp_user"
-    head["time"] = time.time()
-    content = dict()
-    content["head"] = head
-    content["body"] = ""
+    
+    memo_id = request.form["id"]
 
-    memo_temp = content
-    if memo_temp:
-        return render_template("memo_view.html", memo=memo_temp)
+    memo = dict()
+    memo["meta"] = dict()
+    memo["meta"]["title"] = memo_object["meta"]["title"]
+    memo["meta"]["category"] = memo_object["meta"]["category"]
+    memo["meta"]["favorite"] = memo_object["meta"]["favorite"]
+    memo["meta"]["created_time"] = time.time()
+    memo["meta"]["last_edit_time"] = time.time()
+    memo["content"] = memo_object["content"]
+
+    result = memoManager.create_memo(memo_id, memo)
+    if result:
+        return render_template("memo_message.html", message="created")
     else :
         return render_template("memo_message.html", message="failed")
 
-@app.route('/memo_list', methods=['GET'])
-def get_memo_list():
-    # 쿼리 스트링 가져오기 https://stackoverflow.com/questions/11774265/how-do-you-get-a-query-string-on-flask
-    # query_string = request.args.get('from')
-    # query_string = request.args.get('to')
-    result = memo.get_memo_list()
-    return render_template("memo_list.html", memo_list=result)
+# @app.route('/memo_list', methods=['GET'])
+# def get_memo_list():
+#     # 쿼리 스트링 가져오기 https://stackoverflow.com/questions/11774265/how-do-you-get-a-query-string-on-flask
+#     # query_string = request.args.get('from')
+#     # query_string = request.args.get('to')
+#     result = memo.get_memo_list()
+#     return render_template("memo_list.html", memo_list=result)
 
-@app.route('/memo_view', methods=['GET'])
-def get_memo_content():
-    # 쿼리 스트링 가져오기 https://stackoverflow.com/questions/11774265/how-do-you-get-a-query-string-on-flask
-    memo_id = request.args.get('id')
-    result = memo.get_memo_content(memo_id)
-    if result:
-        return render_template("memo_view.html", memo=result)
-    else:
-        return render_template("memo_message.html", message="failed to get memo")
+# @app.route('/memo_view', methods=['GET'])
+# def get_memo_content():
+#     # 쿼리 스트링 가져오기 https://stackoverflow.com/questions/11774265/how-do-you-get-a-query-string-on-flask
+#     memo_id = request.args.get('id')
+#     result = memo.get_memo_content(memo_id)
+#     if result:
+#         return render_template("memo_view.html", memo=result)
+#     else:
+#         return render_template("memo_message.html", message="failed to get memo")
 
-@app.route('/edit_memo', methods=['POST'])
+@app.route('/memo', methods=['PUT'])
 def edit_memo():
-    id_ = request.form["id"]
-    head = dict()
-    head["id"] = id_
-    head["title"] = request.form["title"]
-    head["writer"] = request.form["user"]
-    head["time"] = time.time()
-    content = dict()
-    content["head"] = head
-    content["body"] = request.form["body"]
+    memo_id = request.form["id"]
 
-    result = memo.edit_memo(id_, content)
+    memo = dict()
+    memo["meta"] = dict()
+    memo["meta"]["title"] = memo_object["meta"]["title"]
+    memo["meta"]["category"] = memo_object["meta"]["category"]
+    memo["meta"]["favorite"] = memo_object["meta"]["favorite"]
+    memo["meta"]["created_time"] = memo["meta"]["created_time"]
+    memo["meta"]["last_edit_time"] = time.time()
+    memo["content"] = memo_object["content"]
+
+    result = memo.edit_memo(memo_id, memo)
     if result:
         return render_template("memo_message.html", message="success")
     else:
         return render_template("memo_message.html", message="failed")
 
-@app.route('/delete_memo', methods=['GET'])
+@app.route('/memo', methods=['DELETE'])
 def delete_memo():
     memo_id = request.args.get('id')
     result = memo.delete_memo(memo_id)
--- memo.py
+++ memo.py
@@ -3,23 +3,62 @@ import glob
 import time
 import json
 
-class Memo():
+# {
+#     "meta": {
+#         "title": "new memo",
+#         "category": "aa",
+#         "favorite": true,
+#         "created_time": 1592411559.8346415,
+#         "last_edit_time": 1592411559.8346415
+#     },
+#     "content": "memo is here\r\n"
+# }
+
+memo = dict()
+memo["meta"] = dict()
+memo["meta"]["title"]
+memo["meta"]["category"]
+memo["meta"]["favorite"]
+memo["meta"]["created_time"]
+memo["meta"]["last_edit_time"]
+memo["content"]
+
+
+class MemoManager():
     def __init__(self):
         pass
 
     def get_memo_list(self):
-        memo_head_list = list()
+        memo_list = list()
         memo_path_list = glob.glob("./memo/*.json")
         for memo_path in memo_path_list:
             f = open(memo_path, 'r').read()
-            memo = json.loads(f)
-            memo_head_list.append(memo["head"])
-        return memo_head_list
+            memo_file = json.loads(f)
+
+            memo = dict()
+            memo["meta"] = dict()
+            memo["meta"]["title"] = memo_file["meta"]["title"]
+            memo["meta"]["category"] = memo_file["meta"]["category"]
+            memo["meta"]["favorite"] = memo_file["meta"]["favorite"]
+            memo["meta"]["created_time"] = memo_file["meta"]["created_time"]
+            memo["meta"]["last_edit_time"] = memo_file["meta"]["last_edit_time"]
+            memo["content"] = memo["content"]
+
+            memo_list.append(memo)
+        return memo_list
 
     def get_memo_content(self, ID):
         try:
             f = open("./memo/{}.json".format(ID)).read()
-            memo = json.loads(f)
+            memo_file = json.loads(f)
+            memo = dict()
+            memo["meta"] = dict()
+            memo["meta"]["title"] = memo_file["meta"]["title"]
+            memo["meta"]["category"] = memo_file["meta"]["category"]
+            memo["meta"]["favorite"] = memo_file["meta"]["favorite"]
+            memo["meta"]["created_time"] = memo_file["meta"]["created_time"]
+            memo["meta"]["last_edit_time"] = memo_file["meta"]["last_edit_time"]
+            memo["content"] = memo["content"]
             return memo
         except Exception as identifier:
             print(identifier)
@@ -33,21 +72,42 @@ class Memo():
             print("The file does not exist")
             return False
 
-    def create_memo(self, ID, content):
+    def create_memo(self, ID, memo_object):
         try:
             file_name = "./memo/{}.json".format(ID)
             f = open(file_name, 'w')
-            f.write(content)
+
+            memo = dict()
+            memo["meta"] = dict()
+            memo["meta"]["title"] = memo_object["meta"]["title"]
+            memo["meta"]["category"] = memo_object["meta"]["category"]
+            memo["meta"]["favorite"] = memo_object["meta"]["favorite"]
+            memo["meta"]["created_time"] = time.time()
+            memo["meta"]["last_edit_time"] = time.time()
+            memo["content"] = memo_object["content"]
+            
+            f.write(json.dumps(memo))
             return True
         except Exception as e:
             print(e)
             return False
 
-    def edit_memo(self, ID, content):
+    def edit_memo(self, ID, memo_object):
         try:
-            content = json.dumps(content)
             self.delete_memo(ID)
-            self.create_memo(ID, content)
+            file_name = "./memo/{}.json".format(ID)
+            f = open(file_name, 'w')
+
+            memo = dict()
+            memo["meta"] = dict()
+            memo["meta"]["title"] = memo_object["meta"]["title"]
+            memo["meta"]["category"] = memo_object["meta"]["category"]
+            memo["meta"]["favorite"] = memo_object["meta"]["favorite"]
+            memo["meta"]["created_time"] = memo_object["meta"]["created_time"]
+            memo["meta"]["last_edit_time"] = time.time()
+            memo["content"] = memo_object["content"]
+            
+            f.write(json.dumps(memo))
             return True
         except Exception as identifier:
             print(identifier)
